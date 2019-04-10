3315aa0d981bb5e3a5b2ebf1894394d7cb2df41e
Anurag <anurag2798@gmail.com>
2019-04-10 21:31:29 +0530

    Restructuring with MVC

--- todoez/Controllers/ToDoListViewController.swift
+++ todoez/Controllers/ToDoListViewController.swift
@@ -17,9 +17,9 @@ class ToDoListViewController: UITableViewController {
         let newItem = Item()
         newItem.title = "Anurag"
         itemArray.append(newItem)
-        //if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-        //    itemArray = items
-        //}
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+            itemArray = items
+        }
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -29,12 +29,8 @@ class ToDoListViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         let item = itemArray[indexPath.row]
         cell.textLabel?.text = item.title
-        if item.done == true {
-            cell.accessoryType = .checkmark
-        }
-        else {
-            cell.accessoryType = .none
-        }
+        
+        cell.accessoryType = item.done ? .checkmark : .none
         return cell
     }
     
